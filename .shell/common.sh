18d59d7a394dcb6fc138674283597621cc607fc3
Niclas Roßberger <niclas@nidomiro.de>
2022-10-09 14:05:21 +0200

    feat: adds shell init infrastructure

---
+++ .shell/common.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# gpg
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+
+
+# yarn
+export YARN_REGISTRY='https://registry.yarnpkg.com'
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# set PATH so it includes Android bins if it exists
+if [ -d "$HOME/Android/Sdk/platform-tools" ] ; then
+    PATH="$HOME/Android/Sdk/platform-tools:$PATH"
+fi
---
+++ .shell/macos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+export NVM_DIR="$HOME/.nvm"
+source $(brew --prefix nvm)/nvm.sh
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
