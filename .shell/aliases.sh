a4e5f623daf88ea20b48c1e93a5e867237a4fcd2
Niclas Roßberger <niclas@nidomiro.de>
2023-06-20 20:02:51 +0200

    feat: moves aliases to own file; adds .bashrc

---
+++ .shell/aliases.sh
@@ -0,0 +1,27 @@
+# ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# nx aliases
+alias nx-test-all='nx run-many --target=test --all'
+alias nx-test-affected='nx affected --target=test'
+
+alias nx-test-int-all='nx run-many --target=test-int --all'
+alias nx-int-test-all='nx-test-int-all'
+alias nx-test-int-affected='nx affected --target=test-int'
+alias nx-int-test-affected='nx-test-int-affected'
+
+alias nx-lint-all='nx run-many --target=lint --all'
+alias nx-lint-affected='nx affected --target=lint'
+
+alias nx-type-check-all='nx run-many --target=type-check --all'
+alias nx-type-check-affected='nx affected --target=type-check'
+
+alias nx-build-all='nx run-many --target=build --all'
+alias nx-build-affected='nx affected --target=build'
+
+alias nx-build-all-production='nx run-many --target=build --all --configuration=production'
+alias nx-build-affected-production='nx affected --target=build --configuration=production'
+
+alias nx-auto-generate-mms="npx nx run mint-postman-cli:generate-all-collections && npx nx run mint-postman-cli:cli --job=\"combine-full-dump\" && npx nx affected --target=generate-ocd-runbook --exclude=mint-exceptions && echo \"### Don't forget to update the postman tests if the collection changed! ###\""
--- .shell/common.sh
+++ .shell/common.sh
@@ -15,10 +15,6 @@ gpgconf --launch gpg-agent
 export YARN_REGISTRY='https://registry.yarnpkg.com'
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
-# ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
 
 # set PATH so it includes Android bins if it exists
 if [ -d "$HOME/Android/Sdk/platform-tools" ] ; then
@@ -45,29 +41,8 @@ then
 
 fi
 
-# nx aliases
-alias nx-test-all='nx run-many --target=test --all'
-alias nx-test-affected='nx affected --target=test'
 
-alias nx-test-int-all='nx run-many --target=test-int --all'
-alias nx-int-test-all='nx-test-int-all'
-alias nx-test-int-affected='nx affected --target=test-int'
-alias nx-int-test-affected='nx-test-int-affected'
-
-alias nx-lint-all='nx run-many --target=lint --all'
-alias nx-lint-affected='nx affected --target=lint'
-
-alias nx-type-check-all='nx run-many --target=type-check --all'
-alias nx-type-check-affected='nx affected --target=type-check'
-
-alias nx-build-all='nx run-many --target=build --all'
-alias nx-build-affected='nx affected --target=build'
-
-alias nx-build-all-production='nx run-many --target=build --all --configuration=production'
-alias nx-build-affected-production='nx affected --target=build --configuration=production'
-
-alias nx-auto-generate-mms="npx nx run mint-postman-cli:generate-all-collections && npx nx run mint-postman-cli:cli --job=\"combine-full-dump\" && npx nx affected --target=generate-ocd-runbook --exclude=mint-exceptions && echo \"### Don't forget to update the postman tests if the collection changed! ###\""
-
+. "$HOME/.shell/common.sh"
 
 
 
