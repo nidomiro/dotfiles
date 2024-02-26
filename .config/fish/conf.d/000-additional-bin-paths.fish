effc900a7b26e52df62df6f38a368cb8597232e2
Niclas Roßberger <niclas@nidomiro.de>
2024-02-26 13:38:20 +0100

    feat(fish): add paths; adds parsing of secrets.sh

--- .config/fish/conf.d/000-additional-bin-paths.fish
+++ .config/fish/conf.d/000-additional-bin-paths.fish
@@ -1,14 +1,15 @@
 #!/usr/bin/env fish
 
-# Define an array of paths to be added to the PATH using multiline syntax
-set custom_paths """
-    ~/bin
-"""
 
-# Loop through the custom_paths array and add each directory if it exists
-for directory in (echo $custom_paths)
-    if test -d $directory
-        fish_add_path $directory
+function addToPathIfExists
+    if test -d $argv
+        fish_add_path $argv
     end
 end
+
+
+
+addToPathIfExists ~/bin
+addToPathIfExists /opt/homebrew/bin
+
 
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-
+# Load Secrets
+if test -f ~/.shell/secrets.sh
+    bass source ~/.shell/secrets.sh
+end
 
 
 switch (uname)
