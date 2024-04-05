cfbce17ab25784c351afcff735db5472b2d89a26
Niclas Roßberger <niclas@nidomiro.de>
2024-04-05 10:09:38 +0200

    feat: adds gcloud fish bindings; update mint abbr

--- .config/fish/conf.d/0020-mint.fish
+++ .config/fish/conf.d/0020-mint.fish
@@ -3,4 +3,4 @@
 set --export STRICT_LINTING true
 
 
-abbr --add nx-auto-generate-mint 'npx nx run mint-postman-cli:generate-all-collections && npx nx run mint-postman-cli:cli --job="combine-full-dump" && npx nx affected --target=generate-ocd-runbook --exclude=mint-exceptions && echo "### Don\'t forget to update the postman tests if the collection changed!"'
+abbr --add nx-auto-generate-mint npx nx affected --target=generate-ocd-runbook --exclude=mint-exceptions
---
+++ .config/fish/conf.d/020-gcloud.fish
@@ -0,0 +1,11 @@
+
+
+if test (which brew)
+    set --export GCLOUD_SDK_PATH "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
+end
+
+set GCLOUD_FISH_INCLUDE "$GCLOUD_SDK_PATH/path.fish.inc"
+
+if test -f $GCLOUD_FISH_INCLUDE
+    source $GCLOUD_FISH_INCLUDE
+end
