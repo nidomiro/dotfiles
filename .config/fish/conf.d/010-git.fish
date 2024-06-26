2b3c34aff22c7f4cec8249e864240df657aaa7d4
Niclas Roßberger <niclas@nidomiro.de>
2024-06-26 08:40:59 +0200

    feat(fish): add abbreviations

--- .config/fish/conf.d/010-git.fish
+++ .config/fish/conf.d/010-git.fish
@@ -1,3 +1,6 @@
 
 
 abbr --add trigger-ci 'git pull && git commit --no-verify --allow-empty -m "trigger ci" && git push'
+
+abbr --add gru 'git remote update'
+abbr --add g 'git'
---
+++ .config/fish/conf.d/010-kubectl.fish
@@ -0,0 +1,2 @@
+
+abbr --add k 'kubectl'
