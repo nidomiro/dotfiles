6e2ebc05bbd7465d58c317ef124ffc83fac05a49
Niclas Roßberger <niclas@nidomiro.de>
2024-07-07 20:52:13 +0200

    feat: adds new git abbr / alias

--- .config/fish/conf.d/010-git.fish
+++ .config/fish/conf.d/010-git.fish
@@ -2,5 +2,8 @@
 
 abbr --add trigger-ci 'git pull && git commit --no-verify --allow-empty -m "trigger ci" && git push'
 
-abbr --add gru 'git remote update'
 abbr --add g 'git'
+
+abbr --add gru 'git remote update'
+
+
