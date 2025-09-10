a20643009ecde14e4254abe92c77fe1b92c2ef6c
Niclas Roßberger <niclas@nidomiro.de>
2025-09-10 13:30:32 +0200

    feat: add npm auth script for gcp

--- fish/.config/fish/conf.d/020-mint.fish
+++ fish/.config/fish/conf.d/020-mint.fish
@@ -4,3 +4,8 @@ set --export STRICT_LINTING true
 
 
 abbr --add nx-auto-generate-mint npx nx affected --target=generate-ocd-runbook --exclude=mint-exceptions
+
+function refresh_mint_npm_gcp_token
+    set -x NPM_MINT_GCP_TOKEN (gcloud auth print-access-token)
+    echo "NPM_MINT_GCP_TOKEN is set." 
+end
