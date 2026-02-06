b32f717a135fff91b2c1b9fa56332e7e28e1eeca
Niclas Roßberger <niclas@nidomiro.de>
2026-02-06 09:30:36 +0100

    feat: NODE_OPTIONS: remove old space restriction

--- fish/.config/fish/conf.d/020-node.fish
+++ fish/.config/fish/conf.d/020-node.fish
@@ -1,4 +1,5 @@
 
 if not string match -q -- "*--dns-result-order=ipv4first*" $NODE_OPTIONS
-    set -gx NODE_OPTIONS "$NODE_OPTIONS --dns-result-order=ipv4first --max-old-space-size=1024"
+    # set -gx NODE_OPTIONS "$NODE_OPTIONS --dns-result-order=ipv4first --max-old-space-size=1024"
+    set -gx NODE_OPTIONS "$NODE_OPTIONS --dns-result-order=ipv4first"
 end
